3f1b1007c2154e4598704822d21f50418aaf45a4
LaMalintzin <34371323+MartinGyllstrom@users.noreply.github.com>
2023-05-12 17:23:38 +0200

    Finished functions to make the spins and print

--- main.py
+++ main.py
@@ -1,7 +1,59 @@
+import random
 # Global constant variable
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+# Dictionary which represent the symbols of the reels 
+# and how big are the chances that they will come up, where D will be the most common symbol. 
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+# Function which randomly picks rows and columns, the function generates which symbols that will be in each column 
+# based on the frequency of symbols in the symbol_count dictinary! 
+# By creating a list with all the possible values we could select from and then randomly chosse 3 of those values. 
+# Each value chosen is removed from the list.
+
+
+def get_slot_machine_spin(rows, cols, symbols):
+    # List with the symbols
+    all_symbols = []
+    #for-loop adding the symbols to the List
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    # columns = [[], [], []]
+    # Here we choose which values should go in every single column. 
+    columns = []
+    for _ in range(cols):
+        column = []
+        # Making a copy of the symbols list. This sign [:] called the slice-operator is used so the copy does affect the original. 
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            # Adding the value to the column
+            column.append(value)
+        
+        columns.append(column)
+    
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) -1:
+                print(columns[row], "|")
+            else:
+                print(column[row])
 
 # Function which asks how much the player wants to deposit 
 def deposit():
@@ -66,6 +118,9 @@ def main():
             break
 
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 main()
 
