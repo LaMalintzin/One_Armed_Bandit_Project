3f7537e4721db54a7cd7f51c97a4c625b1555d65
LaMalintzin <34371323+MartinGyllstrom@users.noreply.github.com>
2023-05-15 05:25:46 +0200

    Just a commit

--- main.py
+++ main.py
@@ -51,9 +51,11 @@ def print_slot_machine(columns):
     for row in range(len(columns[0])):
         for i, column in enumerate(columns):
             if i != len(columns) -1:
-                print(columns[row], "|")
+                print(column[row], end = " | ")
             else:
-                print(column[row])
+                print(column[row], end = "")
+        
+        print()
 
 # Function which asks how much the player wants to deposit 
 def deposit():
